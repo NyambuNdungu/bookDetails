99e15db3a244806bd84f85c52cffb8086d73a8a5
Nyamzie <Nyamburafndungu@gmail.com>
2023-01-16 22:05:36 +0300

    set up

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.1"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.graphql-java.tutorial"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework:spring-webflux")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
